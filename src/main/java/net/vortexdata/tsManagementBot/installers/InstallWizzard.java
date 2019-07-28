47b019234879833d59239284401487a5537b89f6
Sandro Kierner <sandro.kierner@gmail.com>
2019-07-28 17:50:09 +0200

    Updated InstallWizzard

--- src/main/java/net/vortexdata/tsManagementBot/installers/InstallWizzard.java
+++ src/main/java/net/vortexdata/tsManagementBot/installers/InstallWizzard.java
@@ -7,7 +7,9 @@ public class InstallWizzard {
     }
 
     public void init() {
-
+        System.out.println("You've started the program in setup mode.");
+        System.out.println("-----------------------------------------");
+        System.out.println("The address of your server can be an IP address or domain (e.g. 'vortexdata.net' or '127.0.0.1'). If your Teamspeak server runns on the same ");
     }
 
 }
