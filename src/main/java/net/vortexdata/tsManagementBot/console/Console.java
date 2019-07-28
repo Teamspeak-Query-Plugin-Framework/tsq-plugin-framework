e81d0c8c627add39846d80d0160915b5f583b77c
Sandro Kierner <sandro.kierner@gmail.com>
2019-07-28 22:27:30 +0200

    Update

--- src/main/java/net/vortexdata/tsManagementBot/console/Console.java
+++ src/main/java/net/vortexdata/tsManagementBot/console/Console.java
@@ -1,12 +1,21 @@
 package net.vortexdata.tsManagementBot.console;
 
+import java.util.*;
+
 public class Console {
 
     public void run() {
 
-    }
+        Scanner scanner = new Scanner(System.in);
+        boolean isSessionActive = true;
+        String inputRaw = "";
+        do {
+            System.out.print("> ");
+            inputRaw = scanner.nextLine();
 
-    public void evaluateCommand() {
+            // Lookup command
+
+        } while (isSessionActive);
 
     }
 
