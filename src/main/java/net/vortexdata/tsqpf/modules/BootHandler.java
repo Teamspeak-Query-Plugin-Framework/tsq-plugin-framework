d9be395fdde68a3aa60bb4843a7dd03d1d1804ba
TAXSET <sandro.kierner@gmail.com>
2019-10-01 15:01:26 +0200

    Added JavaDoc for BootHandler

--- src/main/java/net/vortexdata/tsqpf/modules/BootHandler.java
+++ src/main/java/net/vortexdata/tsqpf/modules/BootHandler.java
@@ -1,18 +1,33 @@
 package net.vortexdata.tsqpf.modules;
 
+/**
+ * Records statistics during boot process
+ *
+ * @author Sandro Kierner
+ * @since 1.0.0
+ */
 public class BootHandler {
 
     private long bootStartTime = 0;
     private long bootEndTime = 0;
 
+    /**
+     * Sets the timestamp of when the boot process started.
+     */
     public void setBootStartTime() {
         bootStartTime = System.currentTimeMillis();
     }
 
+    /**
+     * Sets the timestamp of when the boot process finished.
+     */
     public void setBootEndTime() {
         bootEndTime = System.currentTimeMillis();
     }
 
+    /**
+     * @return Returns the total time that passed during boot
+     */
     public float getBootTime() {
         return bootEndTime - bootStartTime;
     }
