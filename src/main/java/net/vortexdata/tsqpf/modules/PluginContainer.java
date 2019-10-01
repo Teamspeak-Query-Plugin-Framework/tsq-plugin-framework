a61a9d3200899b3615f95932490de28b17f30cfe
TAXSET <sandro.kierner@gmail.com>
2019-10-01 13:14:17 +0200

    Added PluginContainer documentation

--- src/main/java/net/vortexdata/tsqpf/modules/PluginContainer.java
+++ src/main/java/net/vortexdata/tsqpf/modules/PluginContainer.java
@@ -2,6 +2,12 @@ package net.vortexdata.tsqpf.modules;
 
 import net.vortexdata.tsqpf.Framework;
 
+/**
+ * Wrapper class combining all plugin essentials like config, logger, api and co.
+ *
+ * @author Michael Wiesinger
+ * @since 1.0.0
+ */
 public class PluginContainer {
 
     private PluginInterface _pluginInterface;
@@ -15,24 +21,40 @@ public class PluginContainer {
         _pluginConfig = new PluginConfig(_pluginName);
     }
 
+    /**
+     * Initializes the plugins logger class
+     *
+     * @param _Framework            The Frameworks main class
+     */
     public void initLogger(Framework _Framework) {
         if(_logger != null) return;
         _logger = new PluginLogger(_Framework, this);
     }
 
-
+    /**
+     * Returns the plugin interface
+     */
     public PluginInterface getPluginInterface() {
         return _pluginInterface;
     }
 
+    /**
+     * Returns the plugins name
+     */
     public String getPluginName() {
         return _pluginName;
     }
 
+    /**
+     * Returns the plugins logger
+     */
     public PluginLogger getLogger() {
         return _logger;
     }
 
+    /**
+     * Returns the plugins config
+     */
     public PluginConfig getPluginConfig() {
         return _pluginConfig;
     }
