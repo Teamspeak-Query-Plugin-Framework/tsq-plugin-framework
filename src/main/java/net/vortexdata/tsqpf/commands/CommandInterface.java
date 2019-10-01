161317417527a2b3f85f0f81ed6f0a9d0ed2f087
TAXSET <sandro.kierner@gmail.com>
2019-10-01 15:30:27 +0200

    Added JavaDoc for CommandInterface

--- src/main/java/net/vortexdata/tsqpf/commands/CommandInterface.java
+++ src/main/java/net/vortexdata/tsqpf/commands/CommandInterface.java
@@ -2,6 +2,12 @@ package net.vortexdata.tsqpf.commands;
 
 import net.vortexdata.tsqpf.console.Logger;
 
+/**
+ * Interface for Commands
+ *
+ * @author Michael Wiesinger
+ * @since 1.0.0
+ */
 public abstract class CommandInterface {
 
     private Logger _logger;
@@ -13,7 +19,21 @@ public abstract class CommandInterface {
     protected Logger getLogger() {
         return _logger;
     }
+
+    /**
+     * This message is shown with the help command.
+     * @return      Desired help message.
+     */
     abstract public String getHelpMessage();
+
+    /**
+     * This method is run when the user runs the command.
+     */
     abstract public void gotCalled(String[] args);
+
+    /**
+     * This method is run when the user runs the command.
+     * @return          Prefix of the command.
+     */
     abstract public String getName();
 }
