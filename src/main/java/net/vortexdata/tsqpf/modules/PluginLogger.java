822fbe2c8dd097b0b9b7441a8d1a35b0ddd37b73
TAXSET <sandro.kierner@gmail.com>
2019-10-01 13:00:35 +0200

    Added JavaDoc to PluginLogger

--- src/main/java/net/vortexdata/tsqpf/modules/PluginLogger.java
+++ src/main/java/net/vortexdata/tsqpf/modules/PluginLogger.java
@@ -3,6 +3,13 @@ package net.vortexdata.tsqpf.modules;
 import net.vortexdata.tsqpf.Framework;
 import net.vortexdata.tsqpf.console.Logger;
 
+/**
+ * Wrapper class adding prefixes for logging framework
+ *
+ * @author Michael Wiesinger
+ * @author Sandro Kierner
+ * @since 1.0.0
+ */
 public class PluginLogger extends Logger {
 
     private PluginContainer _container;
@@ -12,21 +19,41 @@ public class PluginLogger extends Logger {
         _container = container;
     }
 
+    /**
+     * Logs a message to console or log file on debug level.
+     *
+     * @param message       Message that gets logged to console or log file.
+     */
     @Override
     public void printDebug(String message) {
         super.printDebug("["+_container.getPluginName()+"] " +message);
     }
 
+    /**
+     * Logs a message to console or log file on error level.
+     *
+     * @param message       Message that gets logged to console or log file.
+     */
     @Override
     public void printError(String message) {
         super.printError("["+_container.getPluginName()+"] " +message);
     }
 
+    /**
+     * Logs a message to console or log file on warning level.
+     *
+     * @param message       Message that gets logged to console or log file.
+     */
     @Override
     public void printWarn(String message) {
         super.printWarn("["+_container.getPluginName()+"] " +message);
     }
 
+    /**
+     * Logs a message to console or log file on info level.
+     *
+     * @param message       Message that gets logged to console or log file.
+     */
     @Override
     public void printInfo(String message) {
         super.printInfo("["+_container.getPluginName()+"] " +message);
