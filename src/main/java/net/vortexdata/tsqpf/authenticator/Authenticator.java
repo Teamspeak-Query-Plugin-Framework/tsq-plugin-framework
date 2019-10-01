65a4777fc52b0720dea340fb605c09969c262c84
TAXSET <sandro.kierner@gmail.com>
2019-10-01 12:52:19 +0200

    Updated JavaDoc in authenticator class

--- src/main/java/net/vortexdata/tsqpf/authenticator/Authenticator.java
+++ src/main/java/net/vortexdata/tsqpf/authenticator/Authenticator.java
@@ -3,6 +3,12 @@ package net.vortexdata.tsqpf.authenticator;
 import net.vortexdata.tsqpf.console.FrameworkLogger;
 import net.vortexdata.tsqpf.exceptions.InvalidCredentialsException;
 
+/**
+ * Manages user authentication
+ *
+ * @author Sandro Kierner
+ * @since 2.0.0
+ */
 public class Authenticator {
 
     private FrameworkLogger logger;
@@ -12,6 +18,13 @@ public class Authenticator {
         userManager = new UserManager(this.logger);
     }
 
+    /**
+     * Returns true if login attempt is successful
+     *
+     * @param username                      The users username / unique identifier
+     * @param password                      The users password in plain text
+     * @return                              true if username and password are correct
+     */
     public boolean authenticate(String username, String password) {
         try {
             userManager.authenticate(username, password);
