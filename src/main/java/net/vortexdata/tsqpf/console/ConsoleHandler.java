4ec33e5d95d58eab1a858e7bafb0a384b446fa9f
TAXSET <sandro.kierner@gmail.com>
2019-10-02 20:24:42 +0200

    Update

--- src/main/java/net/vortexdata/tsqpf/console/ConsoleHandler.java
+++ src/main/java/net/vortexdata/tsqpf/console/ConsoleHandler.java
@@ -4,7 +4,7 @@ import net.vortexdata.tsqpf.authenticator.Authenticator;
 import net.vortexdata.tsqpf.authenticator.User;
 import net.vortexdata.tsqpf.authenticator.UserManager;
 import net.vortexdata.tsqpf.commands.CommandInterface;
-import net.vortexdata.tsqpf.exceptions.InvalidCredentialsException;
+import net.vortexdata.tsqpf.exceptions.*;
 import org.apache.log4j.Level;
 
 import javax.print.attribute.standard.JobOriginatingUserName;
@@ -85,7 +85,11 @@ public class ConsoleHandler implements Runnable {
      */
     public void run() {
 
-        currentUser = authenticator.
+        try {
+            currentUser = userManager.getUser("default");
+        } catch (UserNotFoundException e) {
+            e.printStackTrace();
+        }
 
         boolean sessionActive = true;
         Scanner scanner = new Scanner(System.in);
