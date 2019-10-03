a421d22b26922f4dc9861f40f03469af30741db3
TAXSET <sandro.kierner@gmail.com>
2019-10-03 11:15:05 +0200

    Fixed commandExists variable

--- src/main/java/net/vortexdata/tsqpf/console/ConsoleHandler.java
+++ src/main/java/net/vortexdata/tsqpf/console/ConsoleHandler.java
@@ -124,11 +124,11 @@ public class ConsoleHandler implements Runnable {
                     if (cmd.getName().equalsIgnoreCase(data[0])) {
                         if (cmd.isGroupRequirementMet(currentUser.getGroup())) {
                             cmd.gotCalled(Arrays.copyOfRange(data, 1, data.length));
-                            commandExists = true;
                             break;
                         } else {
                             System.out.println("Permission denied.");
                         }
+                        commandExists = true;
                     }
                 }
                 if (!commandExists) {
