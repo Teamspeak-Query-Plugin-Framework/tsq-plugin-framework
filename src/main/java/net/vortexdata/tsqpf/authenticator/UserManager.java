50d178d5c487b50258ceef37f40bf4088e5a53eb
TAXSET <sandro.kierner@gmail.com>
2019-10-03 08:43:21 +0200

    Added password hashing

--- src/main/java/net/vortexdata/tsqpf/authenticator/UserManager.java
+++ src/main/java/net/vortexdata/tsqpf/authenticator/UserManager.java
@@ -6,6 +6,9 @@ import net.vortexdata.tsqpf.exceptions.UserAlreadyExistingException;
 import net.vortexdata.tsqpf.exceptions.UserNotFoundException;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -243,6 +246,27 @@ public class UserManager {
      */
     public User getUser(String username) throws UserNotFoundException {
         return getUserFromSerializedString(loadUserSerializedData(username));
+    }
+
+    public String getPasswordHash(String text) {
+
+        MessageDigest digest = null;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        byte[] encodedString = digest.digest(
+                text.getBytes(StandardCharsets.UTF_8));
+
+        StringBuffer hexString = new StringBuffer();
+        for (int i = 0; i < encodedString.length; i++) {
+            String hex = Integer.toHexString(0xff & encodedString[i]);
+            if(hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+        return hexString.toString();
+
     }
 
 }
