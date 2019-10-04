b2d2b365216355aa2e80774bce863a16c808b316
VarChar42 <46874921+VarChar42@users.noreply.github.com>
2019-10-04 17:30:38 +0200

    Fixed recursive text event loop

--- src/main/java/net/vortexdata/tsqpf/listeners/GlobalEventHandler.java
+++ src/main/java/net/vortexdata/tsqpf/listeners/GlobalEventHandler.java
@@ -23,6 +23,7 @@ public class GlobalEventHandler implements TS3Listener {
     }
 
     public void onTextMessage(TextMessageEvent textMessageEvent) {
+        if(textMessageEvent.getInvokerId() == ts3Api.whoAmI().getId()) return;
         for (PluginContainer pl : PluginManager.getLoadedPlugins()) {
             pl.getPluginInterface().onTextMessage(textMessageEvent);
         }
@@ -30,66 +31,77 @@ public class GlobalEventHandler implements TS3Listener {
     }
 
     public void onClientJoin(ClientJoinEvent clientJoinEvent) {
+        if(clientJoinEvent.getInvokerId() == ts3Api.whoAmI().getId()) return;
         for (PluginContainer pl : PluginManager.getLoadedPlugins()) {
             pl.getPluginInterface().onClientJoin(clientJoinEvent);
         }
     }
 
     public void onClientLeave(ClientLeaveEvent clientLeaveEvent) {
+        if(clientLeaveEvent.getInvokerId() == ts3Api.whoAmI().getId()) return;
         for (PluginContainer pl : PluginManager.getLoadedPlugins()) {
             pl.getPluginInterface().onClientLeave(clientLeaveEvent);
         }
     }
 
     public void onServerEdit(ServerEditedEvent serverEditedEvent) {
+        if(serverEditedEvent.getInvokerId() == ts3Api.whoAmI().getId()) return;
         for (PluginContainer pl : PluginManager.getLoadedPlugins()) {
             pl.getPluginInterface().onServerEdit(serverEditedEvent);
         }
     }
 
     public void onChannelEdit(ChannelEditedEvent channelEditedEvent) {
+        if(channelEditedEvent.getInvokerId() == ts3Api.whoAmI().getId()) return;
         for (PluginContainer pl : PluginManager.getLoadedPlugins()) {
             pl.getPluginInterface().onChannelEdit(channelEditedEvent);
         }
     }
 
     public void onChannelDescriptionChanged(ChannelDescriptionEditedEvent channelDescriptionEditedEvent) {
+        if(channelDescriptionEditedEvent.getInvokerId() == ts3Api.whoAmI().getId()) return;
         for (PluginContainer pl : PluginManager.getLoadedPlugins()) {
             pl.getPluginInterface().onChannelDescriptionChanged(channelDescriptionEditedEvent);
         }
     }
 
     public void onClientMoved(ClientMovedEvent clientMovedEvent) {
+        if(clientMovedEvent.getInvokerId() == ts3Api.whoAmI().getId()) return;
         for (PluginContainer pl : PluginManager.getLoadedPlugins()) {
             pl.getPluginInterface().onClientMoved(clientMovedEvent);
         }
     }
 
     public void onChannelCreate(ChannelCreateEvent channelCreateEvent) {
+        if(channelCreateEvent.getInvokerId() == ts3Api.whoAmI().getId()) return;
         for (PluginContainer pl : PluginManager.getLoadedPlugins()) {
             pl.getPluginInterface().onChannelCreate(channelCreateEvent);
         }
     }
 
     public void onChannelDeleted(ChannelDeletedEvent channelDeletedEvent) {
+        if(channelDeletedEvent.getInvokerId() == ts3Api.whoAmI().getId()) return;
         for (PluginContainer pl : PluginManager.getLoadedPlugins()) {
             pl.getPluginInterface().onChannelDeleted(channelDeletedEvent);
         }
     }
 
     public void onChannelMoved(ChannelMovedEvent channelMovedEvent) {
+        if(channelMovedEvent.getInvokerId() == ts3Api.whoAmI().getId()) return;
         for (PluginContainer pl : PluginManager.getLoadedPlugins()) {
             pl.getPluginInterface().onChannelMoved(channelMovedEvent);
         }
     }
 
     public void onChannelPasswordChanged(ChannelPasswordChangedEvent channelPasswordChangedEvent) {
+        if(channelPasswordChangedEvent.getInvokerId() == ts3Api.whoAmI().getId()) return;
         for (PluginContainer pl : PluginManager.getLoadedPlugins()) {
             pl.getPluginInterface().onChannelPasswordChanged(channelPasswordChangedEvent);
         }
     }
 
     public void onPrivilegeKeyUsed(PrivilegeKeyUsedEvent privilegeKeyUsedEvent) {
+        if(privilegeKeyUsedEvent.getInvokerId() == ts3Api.whoAmI().getId()) return;
         for (PluginContainer pl : PluginManager.getLoadedPlugins()) {
             pl.getPluginInterface().onPrivilegeKeyUsed(privilegeKeyUsedEvent);
         }
