30228a48838be23430153d3a45760616e5b9c065
TAXSET <sandro.kierner@gmail.com>
2019-11-15 18:15:55 +0100

    Updated CommandFramework

--- src/main/java/net/vortexdata/tsqpf/commands/CommandFramework.java
+++ src/main/java/net/vortexdata/tsqpf/commands/CommandFramework.java
@@ -1,14 +1,12 @@
 package net.vortexdata.tsqpf.commands;
 
 import net.vortexdata.tsqpf.Framework;
-import net.vortexdata.tsqpf.commands.commandFramework.ParameterInterpreter;
 import net.vortexdata.tsqpf.console.Logger;
 import net.vortexdata.tsqpf.console.VirtualTerminal;
 
 public class CommandFramework extends CommandInterface {
 
     private Framework framework;
-    private ParameterInterpreter parameterInterpreter;
 
     public CommandFramework(Logger logger, Framework framework) {
         super(logger);
@@ -28,11 +26,19 @@ public class CommandFramework extends CommandInterface {
 
 
             if (args[1].equalsIgnoreCase("reload")) {
-                framework.sleep();
-            } else if (args[1].equalsIgnoreCase("stop")) {
+                terminal.println("This action is not supported in this build.");
+            } else if (args[1].equalsIgnoreCase("stop") || args[1].equalsIgnoreCase("exit") || args[1].equalsIgnoreCase("shutdown")) {
                 framework.shutdown();
             } else if (args[1].equalsIgnoreCase("kill")) {
                 System.exit(0);
+            } else if (args[1].equalsIgnoreCase("stats")) {
+                terminal.println("Available cores: \t\t\t" + Runtime.getRuntime().availableProcessors());
+                terminal.println("Memory usage: \t\t\t" + (Runtime.getRuntime().maxMemory() - Runtime.getRuntime().maxMemory()));
+                terminal.println("Operating system: \t\t\t" + System.getProperty("os.name"));
+                terminal.println("OS version: \t\t\t\t" + System.getProperty("os.version"));
+                terminal.println("Java version: \t\t\t" + System.getProperty("java.version"));
+                terminal.println("Java vendor: \t\t\t" + System.getProperty("java.vendor"));
+
             }
 
 
