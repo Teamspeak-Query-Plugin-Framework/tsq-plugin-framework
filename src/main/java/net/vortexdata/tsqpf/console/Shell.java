9eefd7f3a148c1a98ca0139b62ee46571145844f
TAXSET <sandro.kierner@gmail.com>
2019-12-08 21:44:22 +0100

    Added clause for unknown command feedback

--- src/main/java/net/vortexdata/tsqpf/console/Shell.java
+++ src/main/java/net/vortexdata/tsqpf/console/Shell.java
@@ -88,7 +88,8 @@ public class Shell implements IShell {
 			if(rawCommand.length < 1) continue;
 			CommandInterface command = CommandContainer.searchCommand(rawCommand[0]);
 			if (command == null) {
-				printer.println(rawCommand[0] + ": command not found");
+				if (!rawCommand[0].isEmpty())
+					printer.println(rawCommand[0] + ": command not found");
 				return false;
 			}
 			boolean hasPermission;
