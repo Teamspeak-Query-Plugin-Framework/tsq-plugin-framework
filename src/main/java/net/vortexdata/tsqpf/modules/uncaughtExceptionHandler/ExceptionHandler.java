22e29371226b2cc6dda86c4e3bbc504c2210ecc7
VarChar42 <46874921+VarChar42@users.noreply.github.com>
2019-12-26 20:49:20 +0100

    Added ExceptionHandler

---
+++ src/main/java/net/vortexdata/tsqpf/modules/uncaughtExceptionHandler/ExceptionHandler.java
@@ -0,0 +1,24 @@
+package net.vortexdata.tsqpf.modules.uncaughtExceptionHandler;
+
+import net.vortexdata.tsqpf.Framework;
+import net.vortexdata.tsqpf.FrameworkContainer;
+import net.vortexdata.tsqpf.console.Logger;
+
+public class ExceptionHandler implements Thread.UncaughtExceptionHandler {
+
+
+    private Framework framework;
+    private FrameworkContainer frameworkContainer;
+
+    public ExceptionHandler(Framework framework, FrameworkContainer frameworkContainer) {
+        this.framework = framework;
+        this.frameworkContainer = frameworkContainer;
+    }
+
+
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        System.out.println("["+t.getName()+"] Exception: "+ e.getMessage());
+    }
+}
